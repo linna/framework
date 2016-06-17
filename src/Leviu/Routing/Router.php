0cda0194547660dbbb06f8e3937cee29f2cb6e54
Sebastian Rapetti <sebastian.rapetti@alice.it>
2016-06-17 16:02:27 +0200

    CS fix

--- src/Leviu/Routing/Router.php
+++ src/Leviu/Routing/Router.php
@@ -1,28 +1,27 @@
 <?php
 
 /**
- * Leviu
+ * Leviu.
  *
  * This work would be a little PHP framework, a learn exercice. 
  * 
  * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
  * @copyright (c) 2015, Sebastian Rapetti
  * @license http://opensource.org/licenses/MIT MIT License
+ *
  * @version 0.1.0
  */
-
 namespace Leviu\Routing;
 
 /**
  * Router
  * - Class for manage routes, verify every resource requested by browser and return
- * a RouteInterface Object 
+ * a RouteInterface Object.
  * 
  * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
  */
 class Router
 {
-
     /**
      * @var object Utilized for return the most recently parsed route
      */
@@ -42,31 +41,32 @@ class Router
      * @var array List of regex for find parameter inside passed routes
      */
     protected $matchTypes = array(
-        '`\[[0-9A-Za-z]+\]`'//,
-            // '`\[string:[0-9A-Za-z]+\]`'
+        '`\[[0-9A-Za-z]+\]`',
     );
 
     /**
      * @var array List of regex for find type of parameter inside passed routes
      */
     protected $types = array(
-        // '[0-9]++',
-        '[0-9A-Za-z]++'
+
+        '[0-9A-Za-z]++',
     );
 
     /**
-     * @var string Request uri from browser (start from $_SERVER['REQUEST_URI'])
+     * @var string Request uri from browser (start from['REQUEST_URI'])
      */
     protected $currentUri = '';
 
     /**
-     * Router constructor
+     * Router constructor.
      * 
-     * @param array $routes list of registerd routes for the app in routes.php
+     * @param array  $routes   list of registerd routes for the app in routes.php
      * @param string $basePath Directory of app from config.php
+     *
      * @since 0.1.0
      */
-    public function __construct($routes, $basePath = '') {
+    public function __construct($routes, $basePath = '')
+    {
         //set basePath
         $this->basePath = $basePath;
 
@@ -81,15 +81,17 @@ class Router
     }
 
     /**
-     * getRoute
+     * getRoute.
      * 
      * This method check if a route is valid and 
      * return the route object else return a bad route object
      * 
      * @return \App_mk0\BadRoute|\App_mk0\Route
+     *
      * @since 0.1.0
      */
-    public function getRoute() {
+    public function getRoute()
+    {
 
         //check if current route is valide
         switch ($this->route) {
@@ -107,7 +109,7 @@ class Router
     }
 
     /**
-     * match
+     * match.
      * 
      * This method check if the requested uri is a valid route
      *
@@ -121,9 +123,11 @@ class Router
      * 'matches' => string '/' (length=1)
      * 
      * @return array|null Array contains properties of route
+     *
      * @since 0.1.0
      */
-    protected function match() {
+    protected function match()
+    {
 
         //declare var
         $validRoute = null;
@@ -143,7 +147,7 @@ class Router
 
             //debug
             //var_dump($matches);
-            
+
             //debug
             //var_dump(sizeof($matches));
 
@@ -178,46 +182,48 @@ class Router
 
         //debug
         //var_dump($validRoute);
-        
+
         //return route
         return $validRoute;
     }
 
     /**
-     * buildParam
+     * buildParam.
      * 
      * Try to find param in a valid route
      * 
      * @param array $validRoute Array with route caracteristics
+     *
      * @return array Array with param passed from uri
+     *
      * @since 0.1.0
      */
-    protected function buildParam($validRoute) {
+    protected function buildParam($validRoute)
+    {
         // var_dump($validRoute);
         // if (isset($validRoute['matches'][1]))
         // {
         //    $validRoute['url'] = preg_replace('`\([0-9A-Za-z\|]++\)`', $validRoute['matches'][1], $validRoute['url']);
         //  $matches = explode('/', $validRoute['matches'][1]);
         // }
-        
+
         //debug
         //var_dump($validRoute['url']);
 
         $url = explode('/', $validRoute['url']);
         $matches = explode('/', $validRoute['matches'][0]);
-        
+
         //debug
         //var_dump($url);
         //var_dump($matches);
-        
+
         //old code
         //$c = 1;
         //$j = sizeof($url);
-        
-        
+
         $param = array();
         $rawParam = array_diff($matches, $url);
-        
+
         //debug
         //var_dump(array_diff($matches,$url));
 
@@ -226,7 +232,7 @@ class Router
 
             $param[$paramName] = $value;
         }
-        
+
         //previsous code :)
         /*
         while ($c < $j) {
@@ -251,7 +257,7 @@ class Router
             $c++;
         }
          */
-        
+
         //debug   
         //var_dump($param);
 
@@ -259,18 +265,19 @@ class Router
     }
 
     /**
-     * getCurrentUri
+     * getCurrentUri.
      * 
      * Analize $_SERVER['REQUEST_URI'] for current uri, sanitize and return it
      * 
      * @return string Uri from browser
+     *
      * @since 0.1.0
      */
-    protected function getCurrentUri() {
+    protected function getCurrentUri()
+    {
         $url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
         $url = filter_var($url, FILTER_SANITIZE_URL);
 
-        return '/' . substr($url, strlen($this->basePath));
+        return '/'.substr($url, strlen($this->basePath));
     }
-
 }
