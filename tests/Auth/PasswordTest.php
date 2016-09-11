418fcd8eb18e128eebe1cbdc09377aee498c32e3
Sebastian Rapetti <sebastian.rapetti@alice.it>
2016-09-11 20:49:53 +0200

    PasswordTest added

---
+++ tests/Auth/PasswordTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Linna Framework
+ *
+ * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
+ * @copyright (c) 2016, Sebastian Rapetti
+ * @license http://opensource.org/licenses/MIT MIT License
+ *
+ */
+
+use Linna\Auth\Password;
+
+class PasswordTest extends PHPUnit_Framework_TestCase
+{
+    public function testHash()
+    {
+        $password = new Password();
+        
+        $hash = $password->hash('password');
+        
+        $result = $password->verify('password', $hash);
+        
+        $this->assertEquals(true, $result);
+    }
+    
+    public function testHashFail()
+    {
+        $password = new Password();
+        
+        $hash = $password->hash('password');
+        
+        $result = $password->verify('otherpassword', $hash);
+        
+        $this->assertEquals(false, $result);
+    }
+    
+    public function testNeedRehash()
+    {
+        $password = new Password();
+        
+        $options = [
+            'cost' => 9
+        ];
+        
+        $hash = password_hash('password', PASSWORD_DEFAULT, $options);
+        
+        $result = $password->needsRehash($hash);
+        
+        $this->assertEquals(true, $result);
+    }
+    
+    public function testNoNeedRehash()
+    {
+        $password = new Password();
+        
+        $hash = $password->hash('password');
+        
+        $result = $password->needsRehash($hash);
+        
+        $this->assertEquals(false, $result);
+    }
+}
