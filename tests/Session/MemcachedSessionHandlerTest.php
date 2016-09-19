3040a541a85b90b9a0df384c36a014e25c63ee14
Sebastian Rapetti <sebastian.rapetti@alice.it>
2016-09-19 14:32:20 +0200

    MemcachedSessionHandlerTest added

---
+++ tests/Session/MemcachedSessionHandlerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Linna Framework
+ *
+ * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
+ * @copyright (c) 2016, Sebastian Rapetti
+ * @license http://opensource.org/licenses/MIT MIT License
+ *
+ */
+
+use Linna\Session\MemcachedSessionHandler;
+use Linna\Session\Session;
+use PHPUnit\Framework\TestCase;
+
+class MemcachedSessionHandlerTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSession()
+    {
+        //create memcached instance
+        $memcached = new Memcached();
+        //connect to memcached server
+        $memcached->addServer('localhost', 11211);
+        
+        $sessionHandler = new MemcachedSessionHandler($memcached);
+
+        Session::setSessionHandler($sessionHandler);
+        //se session options
+        Session::withOptions(array(
+            'expire' => 8,
+            'cookieDomain' => '/',
+            'cookiePath' => '/',
+            'cookieSecure' => false,
+            'cookieHttpOnly' => true
+        ));
+        
+        $session = Session::getInstance();
+
+        $session->testdata = 'test';
+
+        $this->assertEquals('test', $session->testdata);
+
+        $session->testdata = 'new test';
+
+        $this->assertEquals('new test', $session->testdata);
+
+        unset($session->testdata);
+
+        $this->assertEquals(false, $session->testdata);
+
+        session_write_close();
+    }
+    
+    /**
+     * @runInSeparateProcess
+     */
+    public function testGc()
+    {
+        //create memcached instance
+        $memcached = new Memcached();
+        //connect to memcached server
+        $memcached->addServer('localhost', 11211);
+        
+        $sessionHandler = new MemcachedSessionHandler($memcached);
+
+        $test = $sessionHandler->gc(0);
+        
+        $this->assertEquals(true, $test);
+    }
+}
