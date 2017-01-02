e712efdeb25ffda70322b5f331e1b8a8951e5bad
Sebastian Rapetti <sebastian.rapetti@alice.it>
2017-01-02 20:32:00 +0100

    happy new year

--- tests/FOO/FOOTemplate.php
+++ tests/FOO/FOOTemplate.php
@@ -5,7 +5,7 @@
  *
  *
  * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
- * @copyright (c) 2016, Sebastian Rapetti
+ * @copyright (c) 2017, Sebastian Rapetti
  * @license http://opensource.org/licenses/MIT MIT License
  *
  */
