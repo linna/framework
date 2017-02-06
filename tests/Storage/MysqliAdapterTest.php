0893658e060aca6c3ae33754a40b6a95d377b093
Sebastian Rapetti <sebastian.rapetti@alice.it>
2017-02-06 20:29:32 +0100

    test added

---
+++ tests/Storage/MysqliAdapterTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Linna Framework
+ *
+ * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
+ * @copyright (c) 2017, Sebastian Rapetti
+ * @license http://opensource.org/licenses/MIT MIT License
+ *
+ */
+
+declare(strict_types=1);
+
+use Linna\Storage\MysqliAdapter;
+use PHPUnit\Framework\TestCase;
+
+class MysqliAdapterTest extends TestCase
+{
+    public function testConnection()
+    {
+        $mysqliAdapter = new MysqliAdapter(
+            '127.0.0.1',
+            $GLOBALS['pdo_mysql_user'],
+            $GLOBALS['pdo_mysql_password'],
+            'test',
+            3306
+        );
+        
+        $this->assertInstanceOf(mysqli::class, $mysqliAdapter->getResource());
+    }
+    
+    public function connectionDataProvider()
+    {
+        return [
+            ['..', $GLOBALS['pdo_mysql_user'], $GLOBALS['pdo_mysql_password'], 'test', 3306],
+            ['127.0.0.1', '', $GLOBALS['pdo_mysql_password'], 'test', 3306],
+            ['127.0.0.1', $GLOBALS['pdo_mysql_user'], '', 'test', 3306],
+            ['127.0.0.1', $GLOBALS['pdo_mysql_user'], $GLOBALS['pdo_mysql_password'], 'otherdb', 3306],
+            ['127.0.0.1', $GLOBALS['pdo_mysql_user'], $GLOBALS['pdo_mysql_password'], 'test', 3305]
+        ];
+    }
+    
+    /**
+     * @dataProvider connectionDataProvider
+     */
+    public function testFailConnection($host, $user, $password, $database, $port)
+    {
+        $this->expectException(RuntimeException::class);
+        
+        $mysqliAdapter = new MysqliAdapter($host, $user, $password, $database, $port);
+        
+        $resource = $mysqliAdapter->getResource();
+    }
+}
