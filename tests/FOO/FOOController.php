d8cd0f0bdd16721b58577baee99a8a4742fdcac9
Sebastian Rapetti <bauleviaggiante@tiscalinet.it>
2017-02-15 22:32:29 +0000

    Apply fixes from StyleCI

--- tests/FOO/FOOController.php
+++ tests/FOO/FOOController.php
@@ -29,12 +29,12 @@ class FOOController extends Controller
     {
         $this->model->modifyDataFromParam($passedData);
     }
-    
+
     public function before()
     {
         //do nothing
     }
-    
+
     public function after()
     {
         //do nothing
