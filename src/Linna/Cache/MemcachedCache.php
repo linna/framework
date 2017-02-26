d6d32f106455b923cdab10bedc1e424ace9334e4
Sebastian Rapetti <bauleviaggiante@tiscalinet.it>
2017-02-26 19:32:41 +0000

    Apply fixes from StyleCI

--- src/Linna/Cache/MemcachedCache.php
+++ src/Linna/Cache/MemcachedCache.php
@@ -7,16 +7,15 @@
  * @copyright (c) 2017, Sebastian Rapetti
  * @license http://opensource.org/licenses/MIT MIT License
  */
-
 declare(strict_types=1);
 
 namespace Linna\Cache;
 
-use Psr\SimpleCache\CacheInterface;
 use Memcached;
+use Psr\SimpleCache\CacheInterface;
 
 /**
- * PSR-16 Memcached
+ * PSR-16 Memcached.
  */
 class MemcachedCache implements CacheInterface
 {
@@ -24,29 +23,31 @@ class MemcachedCache implements CacheInterface
      * @var object Memcached resource
      */
     private $memcached;
-    
+
     /**
-     * Constructor
+     * Constructor.
+     *
      * @param Memcached $memcached
      */
     public function __construct(Memcached $memcached)
     {
         $this->memcached = $memcached;
     }
-    
+
     /**
      * Fetches a value from the cache.
      *
      * @param string $key     The unique key of this item in the cache.
      * @param mixed  $default Default value to return if the key does not exist.
      *
-     * @return mixed The value of the item from the cache, or $default in case of cache miss.
-     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
+     *                                                   MUST be thrown if the $key string is not a legal value.
+     *
+     * @return mixed The value of the item from the cache, or $default in case of cache miss.
      */
     public function get($key, $default = null)
-    {}
+    {
+    }
 
     /**
      * Persists data in the cache, uniquely referenced by a key with an optional expiration TTL time.
@@ -57,50 +58,54 @@ class MemcachedCache implements CacheInterface
      *                                     the driver supports TTL then the library may set a default value
      *                                     for it or let the driver take care of that.
      *
-     * @return bool True on success and false on failure.
-     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
+     *                                                   MUST be thrown if the $key string is not a legal value.
+     *
+     * @return bool True on success and false on failure.
      */
     public function set($key, $value, $ttl = null)
-    {}
-    
+    {
+    }
+
     /**
      * Delete an item from the cache by its unique key.
      *
      * @param string $key The unique cache key of the item to delete.
      *
-     * @return bool True if the item was successfully removed. False if there was an error.
-     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
+     *                                                   MUST be thrown if the $key string is not a legal value.
+     *
+     * @return bool True if the item was successfully removed. False if there was an error.
      */
     public function delete($key)
-    {}
-    
+    {
+    }
+
     /**
      * Wipes clean the entire cache's keys.
      *
      * @return bool True on success and false on failure.
      */
     public function clear()
-    {}
-    
+    {
+    }
+
     /**
      * Obtains multiple cache items by their unique keys.
      *
      * @param iterable $keys    A list of keys that can obtained in a single operation.
      * @param mixed    $default Default value to return for keys that do not exist.
      *
-     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
-     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
+     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
+     *                                                   or if any of the $keys are not a legal value.
+     *
+     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
      */
     public function getMultiple($keys, $default = null)
-    {}
-    
+    {
+    }
+
     /**
      * Persists a set of key => value pairs in the cache, with an optional TTL.
      *
@@ -109,29 +114,31 @@ class MemcachedCache implements CacheInterface
      *                                      the driver supports TTL then the library may set a default value
      *                                      for it or let the driver take care of that.
      *
-     * @return bool True on success and false on failure.
-     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $values is neither an array nor a Traversable,
-     *   or if any of the $values are not a legal value.
+     *                                                   MUST be thrown if $values is neither an array nor a Traversable,
+     *                                                   or if any of the $values are not a legal value.
+     *
+     * @return bool True on success and false on failure.
      */
     public function setMultiple($values, $ttl = null)
-    {}
-    
+    {
+    }
+
     /**
      * Deletes multiple cache items in a single operation.
      *
      * @param iterable $keys A list of string-based keys to be deleted.
      *
-     * @return bool True if the items were successfully removed. False if there was an error.
-     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
+     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
+     *                                                   or if any of the $keys are not a legal value.
+     *
+     * @return bool True if the items were successfully removed. False if there was an error.
      */
     public function deleteMultiple($keys)
-    {}
-    
+    {
+    }
+
     /**
      * Determines whether an item is present in the cache.
      *
@@ -142,11 +149,12 @@ class MemcachedCache implements CacheInterface
      *
      * @param string $key The cache item key.
      *
-     * @return bool
-     *
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
+     *                                                   MUST be thrown if the $key string is not a legal value.
+     *
+     * @return bool
      */
     public function has($key)
-    {}
+    {
+    }
 }
