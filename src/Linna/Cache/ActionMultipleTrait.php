a58c7044768b23b5f9f2ac8419066ebae696f88e
Sebastian Rapetti <sebastian.rapetti@alice.it>
2017-02-28 19:21:37 +0100

    duplicate code to trait :)

---
+++ src/Linna/Cache/ActionMultipleTrait.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * Linna Framework.
+ *
+ * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
+ * @copyright (c) 2017, Sebastian Rapetti
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+declare(strict_types=1);
+
+namespace Linna\Cache;
+
+use DateInterval;
+use DateTime;
+use Linna\Cache\Exception\InvalidArgumentException;
+use Traversable;
+
+/**
+ * ActionMultipleTrait
+ */
+trait ActionMultipleTrait
+{
+    /**
+     * Express Requirements by Abstract Methods.
+     *
+     * @param string $key     The unique key of this item in the cache.
+     * @param mixed  $default Default value to return if the key does not exist.
+     */
+    abstract public function get($key, $default = null);
+    
+    /**
+     * Express Requirements by Abstract Methods.
+     *
+     * @param string                $key   The key of the item to store.
+     * @param mixed                 $value The value of the item to store, must be serializable.
+     * @param null|int|DateInterval $ttl   Optional. The TTL value of this item. If no value is sent and
+     *                                     the driver supports TTL then the library may set a default value
+     *                                     for it or let the driver take care of that.
+     */
+    abstract public function set($key, $value, $ttl);
+    
+    /**
+     * Express Requirements by Abstract Methods.
+     *
+     * @param string $key The cache item key.
+     */
+    abstract public function has($key);
+    
+    /**
+     * Express Requirements by Abstract Methods.
+     *
+     * @param string $key The unique cache key of the item to delete.
+     */
+    abstract public function delete($key);
+    
+    
+    /**
+     * Obtains multiple cache items by their unique keys.
+     *
+     * @param iterable $keys    A list of keys that can obtained in a single operation.
+     * @param mixed    $default Default value to return for keys that do not exist.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
+     *                                                   or if any of the $keys are not a legal value.
+     *
+     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        if (!is_array($keys) && !($keys instanceof Traversable)) {
+            throw new InvalidArgumentException();
+        }
+
+        $result = [];
+        foreach ((array) $keys as $key) {
+            $result[$key] = $this->has($key) ? $this->get($key) : $default;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Persists a set of key => value pairs in the cache, with an optional TTL.
+     *
+     * @param iterable              $values A list of key => value pairs for a multiple-set operation.
+     * @param null|int|DateInterval $ttl    Optional. The TTL value of this item. If no value is sent and
+     *                                      the driver supports TTL then the library may set a default value
+     *                                      for it or let the driver take care of that.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *                                                   MUST be thrown if $values is neither an array nor a Traversable,
+     *                                                   or if any of the $values are not a legal value.
+     *
+     * @return bool True on success and false on failure.
+     */
+    public function setMultiple($values, $ttl = null)
+    {
+        if (!is_array($values) && !($values instanceof Traversable)) {
+            throw new InvalidArgumentException();
+        }
+
+        if ($ttl instanceof DateInterval) {
+            // Converting to a TTL in seconds
+            $ttl = (new DateTime('now'))->add($ttl)->getTimeStamp() - time();
+        }
+
+        foreach ((array) $values as $key => $value) {
+            $this->set($key, $value, $ttl);
+        }
+
+        return true;
+    }
+
+    /**
+     * Deletes multiple cache items in a single operation.
+     *
+     * @param iterable $keys A list of string-based keys to be deleted.
+     *
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
+     *                                                   or if any of the $keys are not a legal value.
+     *
+     * @return bool True if the items were successfully removed. False if there was an error.
+     */
+    public function deleteMultiple($keys)
+    {
+        if (!is_array($keys) && !($keys instanceof Traversable)) {
+            throw new InvalidArgumentException();
+        }
+
+        foreach ((array) $keys as $key) {
+            $this->delete($key);
+        }
+
+        return true;
+    }
+}
+
--- src/Linna/Cache/MemcachedCache.php
+++ src/Linna/Cache/MemcachedCache.php
@@ -16,13 +16,14 @@ use DateTime;
 use Linna\Cache\Exception\InvalidArgumentException;
 use Memcached;
 use Psr\SimpleCache\CacheInterface;
-use Traversable;
 
 /**
  * PSR-16 Memcached.
  */
 class MemcachedCache implements CacheInterface
 {
+    use ActionMultipleTrait;
+        
     /**
      * @var object Memcached resource
      */
@@ -125,89 +126,7 @@ class MemcachedCache implements CacheInterface
     {
         return $this->memcached->flush();
     }
-
-    /**
-     * Obtains multiple cache items by their unique keys.
-     *
-     * @param iterable $keys    A list of keys that can obtained in a single operation.
-     * @param mixed    $default Default value to return for keys that do not exist.
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
-     *                                                   or if any of the $keys are not a legal value.
-     *
-     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
-     */
-    public function getMultiple($keys, $default = null)
-    {
-        if (!is_array($keys) && !($keys instanceof Traversable)) {
-            throw new InvalidArgumentException();
-        }
-
-        $result = [];
-        foreach ((array) $keys as $key) {
-            $result[$key] = $this->has($key) ? $this->get($key) : $default;
-        }
-
-        return $result;
-    }
-
-    /**
-     * Persists a set of key => value pairs in the cache, with an optional TTL.
-     *
-     * @param iterable              $values A list of key => value pairs for a multiple-set operation.
-     * @param null|int|DateInterval $ttl    Optional. The TTL value of this item. If no value is sent and
-     *                                      the driver supports TTL then the library may set a default value
-     *                                      for it or let the driver take care of that.
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *                                                   MUST be thrown if $values is neither an array nor a Traversable,
-     *                                                   or if any of the $values are not a legal value.
-     *
-     * @return bool True on success and false on failure.
-     */
-    public function setMultiple($values, $ttl = null)
-    {
-        if (!is_array($values) && !($values instanceof Traversable)) {
-            throw new InvalidArgumentException();
-        }
-
-        if ($ttl instanceof DateInterval) {
-            // Converting to a TTL in seconds
-            $ttl = (new DateTime('now'))->add($ttl)->getTimeStamp() - time();
-        }
-
-        foreach ((array) $values as $key => $value) {
-            $this->set($key, $value, $ttl);
-        }
-
-        return true;
-    }
-
-    /**
-     * Deletes multiple cache items in a single operation.
-     *
-     * @param iterable $keys A list of string-based keys to be deleted.
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *                                                   MUST be thrown if $keys is neither an array nor a Traversable,
-     *                                                   or if any of the $keys are not a legal value.
-     *
-     * @return bool True if the items were successfully removed. False if there was an error.
-     */
-    public function deleteMultiple($keys)
-    {
-        if (!is_array($keys) && !($keys instanceof Traversable)) {
-            throw new InvalidArgumentException();
-        }
-
-        foreach ((array) $keys as $key) {
-            $this->delete($key);
-        }
-
-        return true;
-    }
-
+    
     /**
      * Determines whether an item is present in the cache.
      *
