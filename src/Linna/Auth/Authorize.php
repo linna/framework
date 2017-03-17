9a571c9453e54b1a2b24a0b283107c786a3da971
Sebastian Rapetti <sebastian.rapetti@alice.it>
2017-03-17 22:20:41 +0100

    initial commit

---
+++ src/Linna/Auth/Authorize.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Linna Framework.
+ *
+ * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
+ * @copyright (c) 2017, Sebastian Rapetti
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+declare(strict_types=1);
+
+namespace Linna\Auth;
+
+/**
+ * Class Authorize
+ */
+class Authorize
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        
+    }
+}
