bbca53d66caca80eeaa4a6b7ee81f85fb642e304
Sebastian Rapetti <bauleviaggiante@tiscalinet.it>
2017-04-22 11:56:06 +0000

    Apply fixes from StyleCI

--- tests/Cache/DiskCacheTest.php
+++ tests/Cache/DiskCacheTest.php
@@ -55,15 +55,15 @@ class DiskCacheTest extends TestCase
 
         $this->assertEquals(0, $cacheValue['expires']);
     }
-    
+
     public function testSetTtl()
     {
         $this->cache->set('foo_ttl', [0, 1, 2, 3, 4], 10);
 
         $cacheValue = include '/tmp/'.sha1('foo_ttl').'.php';
-        
+
         $expectedTtl = time() + 10;
-        
+
         $this->assertEquals($expectedTtl, $cacheValue['expires']);
     }
 
@@ -72,9 +72,9 @@ class DiskCacheTest extends TestCase
         $this->cache->set('foo_ttl', [0, 1, 2, 3, 4], new DateInterval('PT10S'));
 
         $cacheValue = include '/tmp/'.sha1('foo_ttl').'.php';
-        
+
         $expectedTtl = time() + 10;
-        
+
         $this->assertEquals($expectedTtl, $cacheValue['expires']);
     }
 
@@ -220,6 +220,7 @@ class DiskCacheTest extends TestCase
 
         $this->cache->clear();
     }
+
     public function testSetMultipleTtl()
     {
         $this->cache->SetMultiple([
@@ -237,26 +238,26 @@ class DiskCacheTest extends TestCase
         $this->assertEquals(true, file_exists('/tmp/'.sha1('foo_3').'.php'));
         $this->assertEquals(true, file_exists('/tmp/'.sha1('foo_4').'.php'));
         $this->assertEquals(true, file_exists('/tmp/'.sha1('foo_5').'.php'));
-        
+
         $expectedTtl = time() + 10;
-        
+
         $cacheValue0 = include '/tmp/'.sha1('foo_0').'.php';
         $cacheValue1 = include '/tmp/'.sha1('foo_1').'.php';
         $cacheValue2 = include '/tmp/'.sha1('foo_2').'.php';
         $cacheValue3 = include '/tmp/'.sha1('foo_3').'.php';
         $cacheValue4 = include '/tmp/'.sha1('foo_4').'.php';
         $cacheValue5 = include '/tmp/'.sha1('foo_5').'.php';
-        
+
         $this->assertEquals($expectedTtl, $cacheValue0['expires']);
         $this->assertEquals($expectedTtl, $cacheValue1['expires']);
         $this->assertEquals($expectedTtl, $cacheValue2['expires']);
         $this->assertEquals($expectedTtl, $cacheValue3['expires']);
         $this->assertEquals($expectedTtl, $cacheValue4['expires']);
         $this->assertEquals($expectedTtl, $cacheValue5['expires']);
-        
+
         $this->cache->clear();
     }
-    
+
     public function testSetMultipleTtlDateInterval()
     {
         $this->cache->SetMultiple([
@@ -274,23 +275,23 @@ class DiskCacheTest extends TestCase
         $this->assertEquals(true, file_exists('/tmp/'.sha1('foo_3').'.php'));
         $this->assertEquals(true, file_exists('/tmp/'.sha1('foo_4').'.php'));
         $this->assertEquals(true, file_exists('/tmp/'.sha1('foo_5').'.php'));
-        
+
         $expectedTtl = time() + 10;
-        
+
         $cacheValue0 = include '/tmp/'.sha1('foo_0').'.php';
         $cacheValue1 = include '/tmp/'.sha1('foo_1').'.php';
         $cacheValue2 = include '/tmp/'.sha1('foo_2').'.php';
         $cacheValue3 = include '/tmp/'.sha1('foo_3').'.php';
         $cacheValue4 = include '/tmp/'.sha1('foo_4').'.php';
         $cacheValue5 = include '/tmp/'.sha1('foo_5').'.php';
-        
+
         $this->assertEquals($expectedTtl, $cacheValue0['expires']);
         $this->assertEquals($expectedTtl, $cacheValue1['expires']);
         $this->assertEquals($expectedTtl, $cacheValue2['expires']);
         $this->assertEquals($expectedTtl, $cacheValue3['expires']);
         $this->assertEquals($expectedTtl, $cacheValue4['expires']);
         $this->assertEquals($expectedTtl, $cacheValue5['expires']);
-        
+
         $this->cache->clear();
     }
 
