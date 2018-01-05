8bc73adfecbd6ef501d363a74ca24cc57129a293
Sebastian Rapetti <sebastian.rapetti@alice.it>
2018-01-05 17:08:00 +0100

    Happy new year!

--- tests/TestHelper/Mvc/JsonTemplate.php
+++ tests/TestHelper/Mvc/JsonTemplate.php
@@ -5,7 +5,7 @@
  *
  *
  * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
- * @copyright (c) 2017, Sebastian Rapetti
+ * @copyright (c) 2018, Sebastian Rapetti
  * @license http://opensource.org/licenses/MIT MIT License
  */
 declare(strict_types = 1);
--- tests/TestHelper/Mvc/MultipleModel.php
+++ tests/TestHelper/Mvc/MultipleModel.php
@@ -4,7 +4,7 @@
  * Linna Framework.
  *
  * @author Sebastian Rapetti <sebastian.rapetti@alice.it>
- * @copyright (c) 2017, Sebastian Rapetti
+ * @copyright (c) 2018, Sebastian Rapetti
  * @license http://opensource.org/licenses/MIT MIT License
  */
 declare(strict_types = 1);
